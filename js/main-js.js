64961745a9775231f8e2562c5deb8de448a75b52
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-01 17:08:34 -0600

    weather-map-proj updates

--- js/main-js.js
+++ js/main-js.js
@@ -1,15 +1,34 @@
+"use strict";
 
+$(function() {
+
+var weatherDataObj = {};
+
+
+//Open Weather Map Code
 
 $.get("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: OPEN_WEATHER_MAP_KEY,
+    APPID: OPEN_WEATHER_KEY,
     q:     "San Antonio, US",
     units: "imperial"
 }).done(function(data) {
+    weatherDataObj = data;
     console.log(data);
+    console.log(weatherDataObj);
+    // return weatherDataObj;
+    $('#weather-display p').html(
+        'Current Weather' + '<br>' +
+        'Temperature : ' + 'High ' + data.main.temp_max + ' / ' + 'Low ' + data.main.temp_min + ' , ' +
+        'Description : ' + data.weather[0].main + ' , ' +
+        'Humidity : ' + data.main.humidity + ' , ' +
+        'Pressure : ' + data.main.pressure + ' , '
+    );
 });
 
+    // console.log(weatherDataObj);
+
 $.get("http://api.openweathermap.org/data/2.5/onecall", {
-    APPID: OPEN_WEATHER_MAP_KEY,
+    APPID: OPEN_WEATHER_KEY,
     lat:    29.423017,
     lon:   -98.48527,
     units: "imperial"
@@ -17,4 +36,34 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current);
     console.log('A step further - information for tomorrow: ', data.daily[1]);
+    console.log(weatherData);
+});
+
+
+
+
+// MapBox Code
+    mapboxgl.accessToken = MAPBOX_KEY;
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 10,
+    center: [-98.4916, 29.4252]
+});
+
+
+
+// currentWxObj.forEach((item) => {
+//
+//     var Marker = new mapboxgl.Marker()
+//         .setLngLat([-98.4916, 29.4252])
+//         .addTo(map);
+//
+//     var Popup = new mapboxgl.Popup()
+//         .setHTML(item.main, )
+//
+//     Marker.setPopup(Popup)
+// });
+
+
 });
