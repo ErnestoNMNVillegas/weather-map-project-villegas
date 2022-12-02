d98ddf420ef354b08c9c455a267abd7164dec12a
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-02 16:35:22 -0600

    weather-map-proj updates

--- js/main-js.js
+++ js/main-js.js
@@ -2,6 +2,39 @@
 
 $(function() {
 
+// MapBox Code
+    //https://docs.mapbox.com/mapbox-gl-js/example/drag-a-marker/
+    mapboxgl.accessToken = MAPBOX_KEY;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [-98.4916, 29.4260]
+    });
+
+    const marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([-98.4916, 29.4260])
+        .addTo(map);
+
+    function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        coordinates.style.display = 'block';
+        coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+        getFiveDay(lngLat.lat, lngLat.lng);
+        console.log(lngLat);
+        reverseGeocode(lngLat, MAPBOX_KEY).then(function(results) {
+            let markerLocale = ''
+            markerLocale += '<P>' + 'Marker Locatiion: ' + results + '</P>';
+            $('#marker-location').html(markerLocale);
+        });
+    }
+
+
+
+    marker.on('dragend', onDragEnd);
+
 
 //Open Weather Map Code w/ Current Weather Code
 
@@ -10,20 +43,14 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
     q:     "San Antonio, US",
     units: "imperial"
 }).done(function(data) {
-    // weatherDataObj = data;
-    // console.log(data);
-    // console.log(weatherDataObj);
-    // // return weatherDataObj;
     $('#weather-current-display p').html(
-        'Current Weather' + '<br>' +
+        'Current Weather:  San Antonio, TX' + '<br>' +
         'Temperature : ' + 'High ' + data.main.temp_max + ' / ' + 'Low ' + data.main.temp_min + ' , ' +
         'Description : ' + data.weather[0].main + ' , ' +
         'Humidity : ' + data.main.humidity + ' , ' +
-        'Pressure : ' + data.main.pressure + ' , '
+        'Pressure : ' + data.main.pressure + '.'
     );
 });
-
-    // console.log(weatherDataObj);
 
     //One call below not working
 
@@ -39,49 +66,8 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
 //     console.log(weatherData);
 // });
 
-
-
-
-// MapBox Code
-    mapboxgl.accessToken = MAPBOX_KEY;
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 10,
-    center: [-98.4916, 29.4252]
-    });
-
     //San Antonio 5 Day Forecast
-
-    let lat = 29.423017;
-    let long = -98.48527;
-    $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + OPEN_WEATHER_KEY + "&units=imperial").done(function(data) {
-        var reports = data.list;
-        for(let i = 0; i < reports.length; i += 8) {
-            let weatherCards = '';
-            // reports.forEach((value, index) => {
-                // if (index < 5) {
-                    // var date = new Date(value.dt * 1000).toLocaleDateString("en", {
-                    //     weekday: "long",
-                    // });
-                    var icon = reports[i].weather[0].icon;
-                    weatherCards += '<div class="card d-flex fiveDay" style="width: 18rem;">' +
-                        '<div class="card-header">' + 'Day ' + reports[i].dt_txt + '</div>' +
-                        '<ul class="list-group list-group-flush">' +
-                        '<li class="list-group-item">' + 'Temp (High/Low): ' + reports[i].main.temp_max + ' / '+ reports[i].main.temp_min + '</li>' +
-                        '<li class="list-group-item">' + 'Description: ' + reports[i].weather[0].main + ' / ' + reports[i].weather[0].description +  '</li>' +
-                        '<li class="list-group-item">' + 'Humidity: ' + reports[i].main.humidity + '</li>' +
-                        '<li class="list-group-item">' + 'Pressure: ' + reports[i].main.pressure + '</li>' +
-                        '<li class="list-group-item">' + icon + '</li>' +
-                        '</ul>' +
-                        '</div>'
-                    $('#five-day-card').append(weatherCards);
-                // }
-            // });
-            console.log(reports[i]);
-        }
-    });
-
+    getFiveDay(29.4260, -98.4916);
 
     //This is the drop pin function.
     // I obtained this source from https://stackoverflow.com/questions/44430030/mapbox-allow-user-to-click-on-map-and-pin
@@ -89,37 +75,20 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
 
     map.on("click", function(e){
         console.log("background click", e.lngLat);
-        var pinData = {
-            type: "FeatureCollection",
-            features: [{
-                type:"Feature",
-                geometry: { type: "Point", coordinates: [ e.lngLat.lng, e.lngLat.lat ]}
-            }]
-        };
-        map.addSource("pins", {
-            "type": "geojson",
-            "data": pinData
-        });
-        map.addLayer({
-            id: "pinsLayer",
-            type: "circle",
-            source: "pins",
-            paint: {
-                "circle-color": "red",
-                "circle-radius": 5
-            }
-        });
-
         getFiveDay(e.lngLat.lat, e.lngLat.lng);
+        reverseGeocode(e.lngLat, MAPBOX_KEY).then(function(results) {
+            let markerLocale = ''
+            markerLocale += '<P>' + 'Clicked Locatiion: ' + results + '</P>';
+            $('#clicked-location').html(markerLocale);
+        });
     });
 
-
-
+    //5-day forecast function.  I used information from the following post to figure this out:  https://javascript.plainenglish.io/display-7-day-weather-forecast-with-openweather-api-aac8ba21c9e3
     function getFiveDay(lat, lng){
         $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lng +"&appid=" + OPEN_WEATHER_KEY + "&units=imperial").done(function(data) {
             var reports = data.list;
+            let weatherCards = '';
             for(let i = 0; i < reports.length; i += 8) {
-                let weatherCards = '';
                 var icon = reports[i].weather[0].icon;
                 weatherCards += '<div class="card d-flex fiveDay" style="width: 18rem;">' +
                     '<div class="card-header">' + 'Day ' + reports[i].dt_txt + '</div>' +
@@ -131,11 +100,10 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
                     '<li class="list-group-item">' + icon + '</li>' +
                     '</ul>' +
                     '</div>'
-                $('#five-day-card').append(weatherCards);
+                // console.log(reports[i]);
             }
+            $('#five-day-card').html(weatherCards);
         });
-    };
-
-
+    }
 
 });
