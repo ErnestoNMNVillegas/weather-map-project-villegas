ca3b8c323c650e4693e75785c22873930d13ac95
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2022-12-04 18:23:54 -0600

    weather-map-proj updates

--- js/main-js.js
+++ js/main-js.js
@@ -2,10 +2,8 @@
 
 $(function() {
 
+//// MapBox Code ////
 
-
-// MapBox Code
-    //https://docs.mapbox.com/mapbox-gl-js/example/drag-a-marker/
     mapboxgl.accessToken = MAPBOX_KEY;
     var map = new mapboxgl.Map({
         container: 'map',
@@ -14,40 +12,21 @@ $(function() {
         center: [-98.4916, 29.4260]
     });
 
-// Marker Code
-
-    // var marker = new mapboxgl.Marker({
-    //     draggable: true
-    // })
-    //     .setLngLat([-98.4916, 29.4260])
-    //     .addTo(map);
-
-
-
-
-    var geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        marker: {
-            color: 'orange'
-        },
-        mapboxgl: mapboxgl
-    });
-
-    map.addControl(geocoder);
+//// Draggable marker code ////
+// https://docs.mapbox.com/mapbox-gl-js/example/drag-a-marker/
 
     var marker = new mapboxgl
-        .Marker({ draggable: true, color: "pink" })
+        .Marker({ draggable: true, color: "red" })
         .setLngLat([-98.4916, 29.4260])
         .addTo(map)
-    marker.on('dragend', onDragEnd);
 
+    marker.on('dragend', onDragEnd);
 
     function onDragEnd() {
         var lngLat = marker.getLngLat();
         coordinates.style.display = 'block';
         coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
         getFiveDay(lngLat.lat, lngLat.lng);
-        console.log(lngLat);
         reverseGeocode(lngLat, MAPBOX_KEY).then(function(results) {
             let markerLocale = ''
             markerLocale += '<h3>' + '5-Day Forcast Location: ' + results + '</h3>';
@@ -55,72 +34,35 @@ $(function() {
         });
     }
 
+//// Mapbox geocoder code ////
+//This is the source I used to modify the geocode function  https://stackoverflow.com/questions/62411816/add-a-dragable-marker-after-geocoder-result-in-mapbox-gl-js
+
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    map.addControl(geocoder);
 
     geocoder.on('result', function(e) {
         console.log(e.result.center);
         geocoder.clear();
-        // var marker = new mapboxgl
-        //     .Marker({ draggable: true, color: "pink" })
-        //     .setLngLat(e.result.center)
-        //     .addTo(map)
-        //      marker.on('dragend', onDragEnd);
-
         getFiveDay(e.result.center[1], e.result.center[0]);
-        //
-        // function onDragEnd() {
-        //     var lngLat = marker.getLngLat();
-        //     coordinates.style.display = 'block';
-        //     coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
-        //     getFiveDay(lngLat.lat, lngLat.lng);
-        //     console.log(lngLat);
-        //     reverseGeocode(lngLat, MAPBOX_KEY).then(function(results) {
-        //         let markerLocale = ''
-        //         markerLocale += '<P>' + 'Marker Locatiion: ' + results + '</P>';
-        //         $('#marker-location').html(markerLocale);
-        //     });
-        // }
-
-
         let resultObj = {
             lat: '',
             lng: ''
         }
-
         resultObj.lat = e.result.center[1];
         resultObj.lng = e.result.center[0];
-
-        console.log(resultObj);
-
-
         reverseGeocode(resultObj, MAPBOX_KEY).then(function(results) {
             let markerLocale = ''
             markerLocale += '<h3>' + '5-Day Forecast Location: ' + results + '</h3>';
             $('#five-day-forecast-location').html(markerLocale);
         });
-
         return marker.setLngLat(e.result.center)
-
     });
 
-    //Geocode source info https://stackoverflow.com/questions/62411816/add-a-dragable-marker-after-geocoder-result-in-mapbox-gl-js
-
-    // function onDragEnd() {
-    //     const lngLat = marker.getLngLat();
-    //     coordinates.style.display = 'block';
-    //     coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
-    //     getFiveDay(lngLat.lat, lngLat.lng);
-    //     console.log(lngLat);
-    //     reverseGeocode(lngLat, MAPBOX_KEY).then(function(results) {
-    //         let markerLocale = ''
-    //         markerLocale += '<P>' + 'Marker Locatiion: ' + results + '</P>';
-    //         $('#marker-location').html(markerLocale);
-    //     });
-    // }
-
-
-
-
-    //This is the click function code.
+    //Click function code.
     // I used the following source for assistance: https://stackoverflow.com/questions/44430030/mapbox-allow-user-to-click-on-map-and-pin
 
     map.on("click", function(e){
@@ -133,29 +75,23 @@ $(function() {
         });
     });
 
-    $('#user-search-btn').click(function (e){
-        e.preventDefault();
-        console.log("click detected");
-        let text = $("#user-search").val();
-        console.log(text);
-        geocode(text, MAPBOX_KEY).then(function(results) {
-                console.log(results);
-            getFiveDay(results[1], results[0]);
-            function setMarker(results) {
-                marker.setLngLat([results[1], results[0]]);
-                return marker;
-            }
-        })
-    })
-
-    // geocode("15513 Dell Lane, Selma, Texas 78154", MAPBOX_KEY).then(function(results) {
-    //     function setMarker(results) {
-    //         marker.setLngLat([results]);
-    //     }
-    //     console.log(results);
+    // $('#user-search-btn').click(function (e){
+    //     e.preventDefault();
+    //     console.log("click detected");
+    //     let text = $("#user-search").val();
+    //     console.log(text);
+    //     geocode(text, MAPBOX_KEY).then(function(results) {
+    //             console.log(results);
+    //         getFiveDay(results[1], results[0]);
+    //         function setMarker(results) {
+    //             marker.setLngLat([results[1], results[0]]);
+    //             return marker;
+    //         }
+    //     })
     // })
 
-//Open Weather Map Code w/ Current Weather Code
+
+//// Open Weather Map Code w/ Current Weather Code ////
 
 $.get("http://api.openweathermap.org/data/2.5/weather", {
     APPID: OPEN_WEATHER_KEY,
@@ -185,24 +121,31 @@ $.get("http://api.openweathermap.org/data/2.5/weather", {
 //     console.log(weatherData);
 // });
 
-    //San Antonio 5 Day Forecast
+//// San Antonio 5 Day Forecast Code ////
     getFiveDay(29.4260, -98.4916);
 
+    //icon source https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+    //date format source https://stackoverflow.com/questions/64373549/how-do-i-reformat-the-date-from-openweather-api-using-javascript
     //5-day forecast function.  I used information from the following post to figure this out:  https://javascript.plainenglish.io/display-7-day-weather-forecast-with-openweather-api-aac8ba21c9e3
+
     function getFiveDay(lat, lng){
         $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lng +"&appid=" + OPEN_WEATHER_KEY + "&units=imperial").done(function(data) {
             var reports = data.list;
             let weatherCards = '';
             for(let i = 0; i < reports.length; i += 8) {
                 var icon = reports[i].weather[0].icon;
+                var iconurl = "https://openweathermap.org/img/w/" + icon + ".png";
+                $('#wxicon').attr('src', iconurl);
+                var date = new Date(reports[i].dt_txt).toLocaleDateString('en-US');
+                var formatDate = date.split('/').join('-');
                 weatherCards += '<div class="card d-flex fiveDay" style="width: 18rem;">' +
-                    '<div class="card-header">' + 'Day ' + reports[i].dt_txt + '</div>' +
+                    '<div class="card-header">' + 'Day: ' + formatDate + '</div>' +
                     '<ul class="list-group list-group-flush">' +
                     '<li class="list-group-item">' + 'Temp (High/Low): ' + reports[i].main.temp_max + ' / '+ reports[i].main.temp_min + '</li>' +
                     '<li class="list-group-item">' + 'Description: ' + reports[i].weather[0].main + ' / ' + reports[i].weather[0].description +  '</li>' +
                     '<li class="list-group-item">' + 'Humidity: ' + reports[i].main.humidity + '</li>' +
                     '<li class="list-group-item">' + 'Pressure: ' + reports[i].main.pressure + '</li>' +
-                    '<li class="list-group-item">' + icon + '</li>' +
+                    '<li class="list-group-item">' + '<img id="wxicon" src=" " alt="wx icon">' + '</li>' +
                     '</ul>' +
                     '</div>'
                 // console.log(reports[i]);
